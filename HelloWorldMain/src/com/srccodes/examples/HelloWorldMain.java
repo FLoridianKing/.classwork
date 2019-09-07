4e7c86be55dd5945c5770c983f37794a53465c27
FLoridian King <FLoridian King@LAPTOP-S0O47EHJ.attlocal.net>
2019-09-07 19:02:47 -0400

    Initial commit of assignment

---
+++ HelloWorldMain/src/com/srccodes/examples/HelloWorldMain.java
@@ -0,0 +1,13 @@
+package com.srccodes.examples;
+
+public class HelloWorldMain {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
